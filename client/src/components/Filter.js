48bf76aafede9af06b78839cbaed037c03828fd3
Abir Tarchouni <abir.6tarchouni@gmail.com>
2025-03-16 01:07:48 +0100

    updchekhook

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,13 +1,10 @@
 import React from "react"
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import ReactStars from 'react-stars'
+
 
 function Filter({setTitle, setRating}) {
-  const ratingChanged = (newRating) => {
-    console.log(newRating)
-     setRating(newRating)
-  }
+ 
     return (
       <>
        <InputGroup className="mb-3"style={{marginTop:'70px'}} >
@@ -23,17 +20,9 @@ function Filter({setTitle, setRating}) {
         <InputGroup.Text id="inputGroup-sizing-default"style={{color:'cadetblue'}} >
       Movie Rating
         </InputGroup.Text>
-        {/* <Form.Control
-          aria-label="Default" */}
-          {/* // aria-describedby="inputGroup-sizing-default"onChange={(e)=>setRating(e.target.value)} */}
-          <ReactStars
-  count={5}
-  onChange={ratingChanged}
-  size={24}
-  color2={'#ffd700'} 
-  half={true}
-  edit={true}
-  />,
+        <Form.Control
+          aria-label="Default" 
+           aria-describedby="inputGroup-sizing-default"onChange={(e)=>setRating(e.target.value)}  />,
         
       </InputGroup>
       </>
